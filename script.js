6f920a8299ff6da25ee1a581520ebb20a405aead
Chiorean Paul <paul.chiorean08@gmail.com>
2020-01-25 13:32:11 +0200

    adding images and search functionality

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-$.getJSON('data.json', function(data){
-    var output = '<ul class="searchresults">';
-    $.each(data, function(key, val){
-        output += '<li>';
-        output += '<h2>' + val.name + '</h2>';
-        output += '<p>' + val.team + '</p>';
-        output += '<p>' + val.info + '</p>';
-        output += '</li>'
+$('#search').keyup(function() {
+    var searchField = $('#search').val();
+    var myExpresion = new RegExp(searchField, "i");
+    $.getJSON('data.json', function(data){
+        var output = '<ul class="searchresults">';
+        $.each(data, function(key, val){
+            if((val.name.search(myExpresion) != -1) || (val.team.search(myExpresion) != -1)){
+                output += '<li>';
+                output += '<h2>' + val.name + '</h2>';
+                output += '<img src="images/' + val.nickname + '.jpg"/>';
+                output += '<p>Team: ' + '<b>'+ val.team +'</b>' + '</p>';
+                output += '<p>' + val.info + '</p>';
+                output += '</li>'
+            }
+        });
+        output+= '</ul>';
+        $('#update').html(output);
     });
-    output+= '</ul>';
-    $('#update').html(output);
 });
