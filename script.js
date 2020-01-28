5444c0bbd37b69836faa79eb4cd80a566d9f4f0b
Chiorean Paul <paul.chiorean08@gmail.com>
2020-01-28 17:58:17 +0200

    Adding style, and final format

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-$('#search').keyup(function() {
-    var searchField = $('#search').val();
-    var myExpresion = new RegExp(searchField, "i");
-    $.getJSON('data.json', function(data){
-        var output = '<ul class="searchresults">';
-        $.each(data, function(key, val){
-            if((val.name.search(myExpresion) != -1) || (val.team.search(myExpresion) != -1)){
-                output += '<li>';
-                output += '<h2>' + val.name + '</h2>';
-                output += '<img src="images/' + val.nickname + '.jpg"/>';
-                output += '<p>Team: ' + '<b>'+ val.team +'</b>' + '</p>';
-                output += '<p>' + val.info + '</p>';
-                output += '</li>'
-            }
-        });
-        output+= '</ul>';
-        $('#update').html(output);
+$.getJSON('data.json', function(data){
+    var output = '<ul class="searchresults">';
+    $.each(data, function(key, val){
+        output += '<li>';
+        output += '<h2>' + val.name + '</h2>';
+        output += '<p>' + val.team + '</p>';
+        output += '<p>' + val.info + '</p>';
+        output += '</li>'
     });
+    output+= '</ul>';
+    $('#update').html(output);
 });
